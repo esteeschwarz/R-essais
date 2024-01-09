845a07c33d5aea779c314a2c288829209b98faa3
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-09 08:04:17 +0100

    inst out yaml again, try if still available

--- pages/knitpagesrmd.R
+++ pages/knitpagesrmd.R
@@ -3,4 +3,5 @@ library(knitr)
 #?render_markdown()
 #?knit
 #?output_format
-knit("pages/README.Rmd",output="pages/index.html")
+#knit("pages/README.Rmd",output="pages/index.html")
+render("pages/README.Rmd",output_format="html",output_file ="pages/index.html")
