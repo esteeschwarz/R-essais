9e9c468ae6a4ad704cf0cf81f3087ea56f076934
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 19:25:36 +0100

    c1, old libs not loaded from cache

--- pages/knitpagesrmd.R
+++ pages/knitpagesrmd.R
@@ -1,8 +1,8 @@
-library(rmarkdown,lib.loc="rlibs")#,lib.loc = "/usr/local/lib/R/4.3/site-library")
+#library(rmarkdown,lib.loc="rlibs")#,lib.loc = "/usr/local/lib/R/4.3/site-library")
 #library(knitr,lib.loc = "/usr/local/lib/R/4.3/site-library")
 #?render_markdown()
 #?knit
 #?output_format
 #knit("pages/README.Rmd",output="pages/index.html")
-render("pages/wp001.Rmd",output_file ="pages/wp001.html",run_pandoc=T)
+#render("pages/wp001.Rmd",output_file ="pages/wp001.html",run_pandoc=T)
 print("from renderscript")
