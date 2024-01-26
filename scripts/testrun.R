2506f7db0006b5571ab210ae85ea0f193a603c13
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 09:51:28 +0100

    rm from testrun

--- scripts/testrun.R
+++ scripts/testrun.R
@@ -4,10 +4,10 @@ print("testrun")
 #Sys.setenv(R_CRAN_WEB = "https://cloud.r-project.org")
 #install.packages("knitr",contriburl = "https://cloud.r-project.org/src/contrib")
 #install.packages("rmarkdown",contriburl = "https://cloud.r-project.org/src/contrib")
-print("packages installed")
-print(installed.packages())
+#print("packages installed")
+#print(installed.packages())
 #library(jsonlite,lib.loc = "/usr/local/Cellar/r/4.3.2/lib/R/library")
-print(getOption("repos"))
+#print(getOption("repos"))
 #just testing cache, force yaml run
 #cache: run wks.,
 #json chgd, rerun install, now try if cached.
