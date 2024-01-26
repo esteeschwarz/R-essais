3c806d414cee43209a7cf6614b6d7cd5a2cf3979
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 18:56:21 +0100

    false dir

--- pages/knitpagesrmd.R
+++ pages/knitpagesrmd.R
@@ -4,5 +4,5 @@
 #?knit
 #?output_format
 #knit("pages/README.Rmd",output="pages/index.html")
-render("pages/wp001.Rmd",output_file ="pages/wp001.html",run_pandoc=F)
+render("pages/wp001.Rmd",output_file ="pages/wp001.html",run_pandoc=T)
 print("from renderscript")
