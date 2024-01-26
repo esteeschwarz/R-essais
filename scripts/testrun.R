ad36e836b1caab191263184de2845d02391bd6af
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 17:30:05 +0100

    where to written

--- scripts/testrun.R
+++ scripts/testrun.R
@@ -1,5 +1,6 @@
 print(2/100)
 print("testrun")
+list.files()
 samplepage<-'<p>just a small html sample index</p><p><a href="pkg.csv"><packages installed</a></p>'
 writeLines(samplepage,"index.html")
 pkg<-installed.packages()
