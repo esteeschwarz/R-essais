b4f2288a171df950151e706895348bcc6f78fd44
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 18:53:15 +0100

    wks, try render

--- scripts/testrun.R
+++ scripts/testrun.R
@@ -2,12 +2,13 @@ print(2/100)
 print("testrun")
 print(list.files())
 samplepage<-'
-<p>just a small html sample index...:</p><a href="pkg.csv">packages installed</a>'
+<p>just a small html sample index...:</p><a href="pkg.csv">packages installed</a><p><a href="wp001.html>wp001</a></p>'
 writeLines(samplepage,"pages/index.html")
 #install.packages("rmarkdown",lib="rlibs",repos = 'https://cloud.r-project.org')
 #install.packages("markdown",lib="rlibs",repos = 'https://cloud.r-project.org')
 pkg<-installed.packages(lib.loc="rlibs")
 write.csv(pkg,"pages/pkg.csv")
+source("knitpagesrmd.R")
 #dir.create("/home/rlibs")
 #print("packages installed")
 #print(installed.packages())
