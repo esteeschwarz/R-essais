f503c20c11194f10a646d951ba6c10bec4037eb0
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-26 07:04:06 +0100

    upd test action w/ pages

--- pages/knitpagesrmd.R
+++ pages/knitpagesrmd.R
@@ -4,4 +4,4 @@ library(rmarkdown,lib.loc = "/usr/local/lib/R/4.3/site-library")
 #?knit
 #?output_format
 #knit("pages/README.Rmd",output="pages/index.html")
-render("pages/README.Rmd",output_format="html",output_file ="pages/index.html")
+render("pages/wp001.Rmd",output_format="html",output_file ="pages/index.html")
