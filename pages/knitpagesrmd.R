7f96f0e6bba3de202e9774763f2099b9b313023f
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-27 11:03:04 +0100

    c1, libs installed all

--- pages/knitpagesrmd.R
+++ pages/knitpagesrmd.R
@@ -12,5 +12,5 @@ library(jquerylib,lib.loc="/home/runner/work/R-essais/R-essais/rlibs")
 #?output_format
 #knit("pages/README.Rmd",output="pages/index.html")
 setwd("pages")
-render("wp001.Rmd",output_file ="wp001.html",run_pandoc=T)
+#render("wp001.Rmd",output_file ="wp001.html",run_pandoc=T)
 print("from renderscript")
