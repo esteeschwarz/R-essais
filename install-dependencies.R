24b03603086f942b6a7136b3a9caed3b3e35f2a9
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-27 10:53:20 +0100

    wks, now try to inst dep within cached libs

--- install-dependencies.R
+++ install-dependencies.R
@@ -1,10 +1,16 @@
 #install dependencies, called if !cache hit
 dir.create("rlibs")
-print(list.files("rlibs"))
+libs<-list.files("rlibs")
+print(libs)
+
 dep<-read.csv("rdependencies.csv")
+
 rlib<-"rlibs"
 repos<-'https://cloud.r-project.org'
 dep.array<-dep$pkg[dep$chk!=1]
+m<-dep.array%in%libs
+dep.to.install<-dep.array[!m]
+dep.array<-dep.to.install
 for(k in 1:length(dep.array)){
   cat("libs to install:",dep.array[k],"\n")
   cat("suspended\n")
