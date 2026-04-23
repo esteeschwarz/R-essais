a8893d62b1a4fcbd47a453e17c340ae1ff83a36c
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-04-23 06:06:15 +0200

    upd

--- scripts/marginnotesdb.R
+++ scripts/marginnotesdb.R
@@ -1,12 +1,21 @@
-tempfun<-function(){
+get.notes<-function(){
   library(RSQLite)
   d<-dbDriver("SQLite")
   d
   library(DBI)
   con<-dbConnect(d)
   con
-  dbsrc<-"/Users/guhl/Documents/temp/MarginNoteBackup(2025-02-01-13-53-27).marginbackupall"
-  dbsrc<-paste(dbsrc,"MarginNotes.sqlite",sep = "/")
+#  dbsrc<-"/Users/guhl/Documents/temp/MarginNoteBackup(2025-02-01-13-53-27).marginbackupall"
+ # dbsrc<-paste(dbsrc,"MarginNotes.sqlite",sep = "/")
+  dborigin<-"/Users/guhl/Library/Containers/QReader.MarginStudy.easy/Data/Library/Private Documents/MN4NotebookDatabase/0/MarginNotes.sqlite"
+  dborigin<-"/Users/guhl/Library/Containers/QReader.MarginStudy.easy/Data/Library/Private Documents/MN4NotebookDatabase/0/"
+  f<-list.files(dborigin,pattern="MarginNotes.sqlite",full.names=T)
+  f
+  dbcopy<-"~/db/MarginNotes.sqlite"
+  dbcopy<-"~/db/marginnotes/"
+  #file.copy(dborigin,dbcopy)
+  file.copy(f,dbcopy,overwrite=T)
+  dbsrc<-paste0(dbcopy,"MarginNotes.sqlite")    
   #dbListTables(con <- dbConnect(RSQLite::SQLite(), ":memory:"))
   con<-dbConnect(d,dbsrc)
   #con<-dbConnect(d,"/Users/guhl/boxHKW/21S/DH/local/AVL/2024/WIT/2025-01-21_FolioFF.sqlite")
@@ -48,11 +57,48 @@ tempfun<-function(){
   #?abind
   dbnotes<-data.frame(abind(all.notes,along = 1))
   
-  dbnotes.s<-dbnotes[order(dbnotes),]
+  dbnotes.s<-dbnotes[order(dbnotes$doc),]
 
-    dbt<-merge(all.tables,check.rows=F,check.names=F)
-  save(dbnotes.s,file="/Users/guhl/Documents/temp/marginnotescpt15061.RData")
-  m<-grep("Wolf|wolf",dbnotes.s$doc)
-  we<-dbnotes.s[m,]
+    #dbt<-merge(all.tables,check.rows=F,check.names=F)
+  #save(dbnotes.s,file="/Users/guhl/db/marginnotescpt.16172.RData")
+  #save(dbnotes.s,file=paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/marginnotescpt.16172.RData"))
+  #m<-grep("Wolf|wolf",dbnotes.s$doc)
+  #we<-dbnotes.s[m,]
+  return(dbnotes.s)
+}
+#db.sf<-margindb
+fetch.anno<-function(dbsub){
+  load(paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/play/quarto/start/margindb.RData"))
+  #margindb<-dbsub
+  # qa<-list(litKI="Wiener_Einführung.pdf",nietzsche=c("nietzsche, kga 3-1, geburt d tragödie.pdf","nietzsche briefe ggl.pdf"),textur=NA) # margin note studyset
+x<-qa[[1]]
+ql<-lapply(seq_along(qa), function(x){
+  study<-names(qa[x])
+  print(study)
+  pl<-lapply(qa[[x]], function(p){
+    paper<-p
+    print(paper)
+    m1<-grep(paper,margindb$doc)
+    print(length(m1))
+    mi<-grep("#init",margindb$comment)
+    print(margindb$notes[mi])
+    mi<-mi[mi%in%m1]
+    print(margindb$comment[mi])
+    ci<-gsub("#cite_(.*)_","\\1",margindb$comment[mi])
+    print(ci)
+    mt<-grep("#init",margindb$comment)
+    ct<-gsub("#init_(.*)_","\\1",margindb$comment[mt])
+    pn<-margindb[m1,]
+    pn<-pn[order(pn$spage),]
+    pn$title<-ct
+    pn$cite<-ci
+    pn$study<-study
+    return(pn)
+    
+  })
+})
   
-  
+    
+}
+
+
